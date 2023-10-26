825108b304434976fb478aa9da6ecdb7ee8ac6d3
edoug0714 <124632335+edoug0714@users.noreply.github.com>
2023-10-26 12:54:06 -0700

    Add files via upload

--- simulator.py
+++ simulator.py
@@ -1,80 +1,179 @@
-import pygame
-import math
-import pandas as pd
-import numpy as np
+
 from pybaseball import standings
+import pygame
+import test
+import adjust_score
 
-###
-#statcast(start_dt="2019-06-24", end_dt="2019-06-25").columns
-#Index(['pitch_type', 'game_date', 'release_speed', 'release_pos_x',
-              #'release_pos_z', 'player_name', 'batter', 'pitcher', 'events',
-             # 'description', 'spin_dir', 'spin_rate_deprecated',
-             # 'break_angle_deprecated', 'break_length_deprecated', 'zone', 'des',
-             # 'game_type', 'stand', 'p_throws', 'home_team', 'away_team', 'type',
-            #  'hit_location', 'bb_type', 'balls', 'strikes', 'game_year', 'pfx_x',
-             # 'pfx_z', 'plate_x', 'plate_z', 'on_3b', 'on_2b', 'on_1b',
-             # 'outs_when_up', 'inning', 'inning_topbot', 'hc_x', 'hc_y',
-             # 'tfs_deprecated', 'tfs_zulu_deprecated', 'fielder_2', 'umpire', 'sv_id',
-             # 'vx0', 'vy0', 'vz0', 'ax', 'ay', 'az', 'sz_top', 'sz_bot',
-             # 'hit_distance_sc', 'launch_speed', 'launch_angle', 'effective_speed',
-             # 'release_spin_rate', 'release_extension', 'game_pk', 'pitcher.1',
-             # 'fielder_2.1', 'fielder_3', 'fielder_4', 'fielder_5', 'fielder_6',
-             # 'fielder_7', 'fielder_8', 'fielder_9', 'release_pos_y',
-             # 'estimated_ba_using_speedangle', 'estimated_woba_using_speedangle',
-             # 'woba_value', 'woba_denom', 'babip_value', 'iso_value',
-             # 'launch_speed_angle', 'at_bat_number', 'pitch_number', 'pitch_name',
-             # 'home_score', 'away_score', 'bat_score', 'fld_score', 'post_away_score',
-              #'post_home_score', 'post_bat_score', 'post_fld_score',
-             # 'if_fielding_alignment', 'of_fielding_alignment', 'spin_axis',
-             # 'delta_home_win_exp', 'delta_run_exp'],
-            #dtype='object')
-###
-
-def get_data():
-    data = standings(2023)
-    print(data)
-
-
-
+(width, height) = (1920, 1080)
 
 def runner_on_second() -> bool:
     return True
 
-#Creating Window
-pygame.init()
-background = (50, 50, 50)
-(width, height) = (1920, 1080)
-base_size = width / 16
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Tutorial')
-screen.fill(background)
 
-#Draw Field
-dirt = pygame.draw.polygon(screen, (193, 153, 107), [(width / 2, height / 80), (width / 2 - width / 8, height / 80 + width / 8), (width / 2, height / 80 + width / 4), (width / 2 + width / 8, height /80 + width / 8)], 0)
-grass = pygame.draw.polygon(screen, (0, 128, 0), [(width / 2, height / 80 + width / 24 + width / 48), (width / 2 - width / 8 + width / 24 + width / 48, height / 80 + width / 8), (width / 2, height / 80 + width / 4 - width / 24 - width / 48), (width / 2 + width / 8 - width / 24 - width / 48, height / 80 + width / 8)], 0)
-home_plate = pygame.draw.polygon(screen, (255, 255, 255), [(width / 2, height / 80 + width / 4 - width / 96), (width / 2 - width / 72, height / 80 + width / 4 - width / 48), (width / 2 - width / 72, height / 80 + width / 4 - width / 24), (width / 2 + width / 72, height / 80 + width / 4 - width / 24), (width / 2 + width / 72, height / 80 + width / 4 - width / 48)], 0)                                                                                                                       
-third_base = pygame.draw.polygon(screen, (255, 255, 255), [(width / 2 - width / 8 + width / 48 + width / 96, height / 80 + width / 8 - width / 48), (width / 2 - width / 8 + width / 96, height / 80 + width / 8), (width / 2 - width / 8 + width / 48 + width / 96, height / 80 + width / 8 + width / 48), (width / 2 - width / 8 + width / 24 + width / 96, height / 80 + width / 8) ], 0)
-second_base = pygame.draw.polygon(screen, (255, 255, 255), [(width / 2, height / 80 + width / 96), (width / 2 - width / 48, height / 80 + width / 48 + width / 96), (width / 2, height / 80 + width / 24 + width / 96), (width / 2 + width / 48, height / 80 + width / 48 + width / 96)], 0)
-first_base = pygame.draw.polygon(screen, (255, 255, 255), [(width / 2 + width / 8 - width / 48 - width / 96, height / 80 + width / 8 - width / 48), (width / 2 + width / 8 - width / 24 - width / 96, height / 80 + width / 8), (width / 2 + width / 8 - width / 48 - width / 96, height / 80 + width / 8 + width / 48), (width / 2 + width / 8 - width / 96, height / 80 + width / 8)], 0)
-pygame.draw.polygon(screen, (255, 255, 255), [(second_base.center), (first_base.center), (home_plate.center), (third_base.center)], 12) 
-pygame.display.flip()
+
+def create_window():
+    pygame.init()
+    background = (50, 50, 50)
+    base_size = width / 16
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('Tutorial')
+    screen.fill(background)
+
+    return screen
+
+def draw_field():
+    screen = create_window()
+    dirt = pygame.draw.polygon(screen, (193, 153, 107), [(width / 2, height / 80), (width / 2 - width / 8, height / 80 + width / 8), (width / 2, height / 80 + width / 4), (width / 2 + width / 8, height /80 + width / 8)], 0)
+    grass = pygame.draw.polygon(screen, (0, 128, 0), [(width / 2, height / 80 + width / 24 + width / 48), (width / 2 - width / 8 + width / 24 + width / 48, height / 80 + width / 8), (width / 2, height / 80 + width / 4 - width / 24 - width / 48), (width / 2 + width / 8 - width / 24 - width / 48, height / 80 + width / 8)], 0)
+    home_plate = pygame.draw.polygon(screen, (255, 255, 255), [(width / 2, height / 80 + width / 4 - width / 96), (width / 2 - width / 72, height / 80 + width / 4 - width / 48), (width / 2 - width / 72, height / 80 + width / 4 - width / 24), (width / 2 + width / 72, height / 80 +width / 4 - width / 24), (width / 2 + width / 72, height / 80 + width / 4 - width / 48)], 0)                                                                                                                       
+    third_base = pygame.draw.polygon(screen, (255, 255, 255), [(width / 2 - width / 8 + width / 48 + width / 96, height / 80 + width / 8 - width / 48), (width / 2 - width / 8 + width / 96, height / 80 + width / 8), (width / 2 - width / 8 + width / 48 + width / 96, height / 80 + width / 8 + width / 48), (width / 2 - width / 8 + width / 24 + width / 96, height / 80 + width / 8) ], 0)
+    second_base = pygame.draw.polygon(screen, (255, 255, 255), [(width / 2, height / 80 + width / 96), (width / 2 - width / 48, height / 80 + width / 48 + width / 96), (width / 2, height / 80 + width / 24 + width / 96), (width / 2 + width / 48, height / 80 + width / 48 + width / 96)], 0)
+    first_base = pygame.draw.polygon(screen, (255, 255, 255), [(width / 2 + width / 8 - width / 48 - width / 96, height / 80 + width / 8 - width / 48), (width / 2 + width / 8 - width / 24 - width / 96, height / 80 + width / 8), (width / 2 + width / 8 - width / 48 - width / 96, height / 80 + width / 8 + width / 48), (width / 2 + width / 8 - width / 96, height / 80 + width / 8)], 0)
+    pygame.draw.polygon(screen, (255, 255, 255), [(second_base.center), (first_base.center), (home_plate.center), (third_base.center)], 12) 
+    pygame.display.flip()
+
 
 #Game Loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    get_data()
+def simulate():
+    screen = create_window()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
     
-    if runner_on_second():
-        second_base = pygame.draw.polygon(screen, (255, 234, 0), [(width / 2, height / 80 + width / 96), (width / 2 - width / 48, height / 80 + width / 48 + width / 96), (width / 2, height / 80 + width / 24 + width / 96), (width / 2 + width / 48, height / 80 + width / 48 + width / 96)], 0)
-
-
-
-   
-
+        if runner_on_second():
+            second_base = pygame.draw.polygon(screen, (255, 234, 0), [(width / 2, height / 80 + width / 96), (width / 2 - width / 48, height / 80 + width / 48 + width / 96), (width / 2, height / 80 + width / 24 + width / 96), (width / 2 + width / 48, height / 80 + width / 48 + width / 96)], 0)
 
     pygame.display.flip()
 
+
+def main():
+    teams = test.get_user_input()
+
+    #print(teams)
+
+    home_batting = test.get_hitting(teams[0], teams[1])
+    away_batting = test.get_hitting(teams[2], teams[3])
+    home_pitching = test.get_pitching(teams[0], teams[1])
+    away_pitching = test.get_pitching(teams[2], teams[3])
+
+    away_batter_list = away_batting['Name'].tolist()
+    home_batter_list = home_batting['Name'].tolist()
+
+    home_starters = home_pitching[home_pitching['Pos'] == 'SP'].iloc[:5]
+    home_bullpen = home_pitching[home_pitching['Pos'] != 'SP']
+    away_starters = away_pitching[away_pitching['Pos'] == 'SP'].iloc[:5]
+    away_bullpen = away_pitching[away_pitching['Pos'] != 'SP']
+
+    print(home_starters[['Name', 'ERA', 'WHIP']])
+    home_pitcher = input('\nChoose the starting pitcher for the home team:')
+    print(away_starters[['Name', 'ERA', 'WHIP']])
+    away_pitcher = input('\n\nChoose the starting pitcher for the away team:')
+
+    #This for loop is the main driver for the sim
+    dummy = 0
+    #i = 0
+    j = 0
+    k = 0
+    game_over = False
+    home_runs = 0
+    away_runs = 0
+    pitcher_stamina = 100
+    home_strikeout_counter = 0
+    away_strikeout_counter = 0
+    #while game_over == False:
+    for i in range(18):
+        if i % 2 == 0:
+            if i == 0:
+                print('\n\nTOP OF THE 1ST INNING')
+            elif i == 2:
+                print('\n\nTOP OF THE 2ND INNING')
+            elif i == 4:
+                print('\n\nTOP OF THE 3RD INNING')
+            else:
+                print('\n\nTOP OF THE', str(round((i+2) / 2)) + 'TH INNING')
+            
+            #Start of each half inning
+            #[1ST, 2ND, 3RD, RUNS_ON_PLAY]
+            runners = [0, 0, 0, 0, 0]
+            print('Pitching for ' + teams[0] + ': ', home_pitcher)
+            while runners[4] < 3:
+                starting_outs = runners[4]
+                batter = away_batter_list[j]
+                print('Batting for ' + teams[2] + ': ', batter)
+                result = test.at_bat(home_batting, away_batting, home_pitching, away_pitching, home_pitcher, batter, 'Top')
+
+                #If at bat results in a strikeout, display pitcher's strikeout total
+                if result == 'Strikeout':
+                    home_strikeout_counter += 1
+                    print(result + '(' + str(home_strikeout_counter) + ')')
+                else:
+                    print(result)
+
+                runners = adjust_score.adjust_score(runners, result)
+                if runners[3] != 0:
+                    away_runs += int(runners[3])
+                    print(teams[0] + ':', home_runs, '|', teams[2] + ':', away_runs)
+                    runners[3] = 0
+
+                #Increase number of outs when an out occurs
+                if starting_outs != runners[4]:
+                    if runners[4] == 1:
+                        print('1 OUT!')
+                    elif runners[4] == 2:
+                        print('2 OUTS!')
+                    else:
+                        print('3 OUTS!')
+
+                print(runners)
+                print('\n')
+
+                if j == 8:
+                    j = 0
+                else:
+                    j += 1
+
+        else:
+            if i == 1:
+                print('\n\nBOTTOM OF THE 1ST INNING')
+            elif i == 3:
+                print('\n\nBOTTOM OF THE 2ND INNING')
+            elif i == 5:
+                print('\n\nBOTTOM OF THE 3RD INNING')
+            else:
+                print('\n\nBOTTOM OF THE', str(round((i+1) / 2)) + 'TH INNING')
+            runners = [0, 0, 0, 0, 0]
+            print('Pitching for ', teams[2] + ': ', away_pitcher)
+            while runners[4] < 3:
+                starting_outs = runners[4]
+                batter = home_batter_list[k]
+                print('Batting for ', teams[0] + ': ', batter)
+                result = test.at_bat(home_batting, away_batting, home_pitching, away_pitching, away_pitcher, batter, 'Bot')
+                print(result)
+
+                runners = adjust_score.adjust_score(runners, result)
+                if runners[3] != 0:
+                    home_runs += int(runners[3])
+                    print(teams[0] + ':', home_runs, '|', teams[2] + ':', away_runs)
+                    runners[3] = 0
+
+                if starting_outs != runners[4]:
+                    if runners[4] == 1:
+                        print('1 OUT!')
+                    elif runners[4] == 2:
+                        print('2 OUTS!')
+                    else:
+                        print('3 OUTS!')
+
+                print(runners)
+                print('\n')
+
+                if k == 8:
+                    k = 0
+                else:
+                    k += 1
+
+
+
+main()
